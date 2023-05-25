135d6597138640f05046a4ec2d2e6cf15b1c273a
Aryan baliy@n <ankushbaliyan142@gmail.com>
2023-05-25 11:41:56 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import reactlogo from "./react-sticker.jpg";
+import gitlogo from "./git.jpg";
+import npmlogo from "./npm.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Deploy React APP on Github Pages</h1>
+      <div className="logosec">
+        <img src={reactlogo} />
+        <h1>+</h1>
+        <img src={npmlogo} width="225px" />
+        <h1>=</h1>
+        <img src={gitlogo} width="225px" />
+      </div>
     </div>
   );
 }
